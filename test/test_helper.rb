694caeb8256173863d4e6a7e8c682b67a4c60d7d
Martin Samami <martin@digitalkookie.io>
2017-04-18 13:42:30 +0200

    Add some deps :godmode:

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,11 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'bigbank/client'
 
+require 'bigbank/client'
 require 'minitest/autorun'
+require 'webmock/minitest'
+require 'vcr'
+
+VCR.configure do |c|
+  c.cassette_library_dir = "test/cassettes"
+  c.hook_into :webmock
+end
