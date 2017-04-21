08c935debaacb1301fcc1f01929ba77aaa3f633d
Martin Samami ッ <martin@digitalkookie.io>
2017-04-21 17:27:25 +0200

    Fields and Application endpoint (#1)
    * Add circle.yml
    
    * Partial work comitted
    
    * Update README
    
    * Update README
    
    * Update README
    
    * Update README
    
    * Fields method working
    
    * Add application endpoint
    
    * Do not append endpoints
    
    * method_missing and more
    
    * Fix client test
    
    * Update README
    
    * Update README
    
    * Update README
    
    * Application working
    
    * Add #success? and #errors?

--- lib/bigbank/client.rb
+++ lib/bigbank/client.rb
@@ -1,6 +1,14 @@
+require 'faraday'
+require 'faraday_middleware'
+
 require_relative "client/version"
 require_relative "client/error"
 require_relative "client/config"
+require_relative "client/endpoint"
+require_relative "client/result"
+
+require_relative "client/endpoints/field"
+require_relative "client/endpoints/application"
 
 module Bigbank
   module Client
@@ -14,3 +22,5 @@ module Bigbank
     end
   end
 end
+
+Bigbank::Client.configure {}
--- lib/bigbank/client/config.rb
+++ lib/bigbank/client/config.rb
@@ -1,10 +1,27 @@
 module Bigbank
   module Client
     class Config
-      attr_accessor :endpoint
+      attr_accessor :endpoint,
+                    :partner_key,
+                    :open_timeout,
+                    :timeout,
+                    :adapter,
+                    :verify_ssl,
+                    :enable_proxy,
+                    :proxy
 
       def initialize
-        @endpoint = "https://ansok.bigbank.se/api"
+        @endpoint = "https://ansok.bigbank.se/"
+        @open_timeout = 5
+        @timeout = 15
+        @adapter = Faraday.default_adapter
+        @verify_ssl = true
+        @enable_proxy = false
+        @proxy = ""
+      end
+
+      def enable_proxy?
+        !!enable_proxy
       end
     end
   end
